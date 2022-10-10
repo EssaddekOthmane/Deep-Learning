7f24a9a8cdb27059a1196ff9ee006556aaed0530
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-10-10 17:38:35 +0100

    Update DL_1.py

--- DL_1.py
+++ DL_1.py
@@ -17,9 +17,94 @@ st.subheader("2. Application à une base de donnéé avec cibles multiple (2)")
 df=pd.read_excel("Db_aout.xlsx")
 df=df.dropna()
 df = df.sample(frac = 1)
+columns=[ 'S0_F0', 'S1_F0', 'S2_F0', 'S3_F0',
+       'S4_F0', 'S5_F0', 'S6_F0', 'S7_F0', 'S8_F0', 'S9_F0', 'S10_F0',
+       'S11_F0', 'S12_F0', 'S13_F0', 'S14_F0', 'S0_F1', 'S0_F2', 'S0_F3',
+       'S0_F4', 'S1_F1', 'S1_F2', 'S1_F3', 'S1_F4', 'S2_F1', 'S2_F2', 'S2_F3',
+       'S2_F4', 'S3_F1', 'S3_F2', 'S3_F3', 'S3_F4', 'S4_F1', 'S4_F2', 'S4_F3',
+       'S4_F4', 'S5_F1', 'S5_F2', 'S5_F3', 'S5_F4', 'S6_F1', 'S6_F2', 'S6_F3',
+       'S6_F4', 'S7_F1', 'S7_F2', 'S7_F3', 'S7_F4', 'S8_F1', 'S8_F2', 'S8_F3',
+       'S8_F4', 'S9_F1', 'S9_F2', 'S9_F3', 'S9_F4', 'S10_F1', 'S10_F2',
+       'S10_F3', 'S10_F4', 'S11_F1', 'S11_F2', 'S11_F3', 'S11_F4', 'S12_F1',
+       'S12_F2', 'S12_F3', 'S12_F4', 'S13_F1', 'S13_F2', 'S13_F3', 'S13_F4',
+       'S14_F1', 'S14_F2', 'S14_F3', 'S14_F4', 'S0_F5', 'S1_F5', 'S2_F5',
+       'S3_F5', 'S4_F5', 'S5_F5', 'S6_F5', 'S7_F5', 'S8_F5', 'S9_F5', 'S10_F5',
+       'S11_F5', 'S12_F5', 'S13_F5', 'S14_F5']
+
 st.dataframe(df)
+
 st.markdown("Notre base de données est constitué de 90 variables explicatives et deux variables cibles. ")
-st.markdown("On commence par construire le modèle. ")
+st.markdown("On donne le choix à l'utilisateur entre traiter directement les deux variables cibles ou non ")
+cible=st.radio(
+              "voulez vouz traiter directement les deux variables cibles ? ",
+              ('Oui','Non'),key=100000000000)
+if cible=='oui':
+       
+       X_train, X_test = train_test_split(df[columns], test_size=0.2, random_state=42, shuffle=True)
+       trainScaler=StandardScaler()
+
+       y_train, y_test = train_test_split(df[['ref1', 'ref2']], test_size=0.2, random_state=42, shuffle=True)
+       trainScaler=StandardScaler()
+
+
+       X_trainScaler=StandardScaler()
+       X_testScaler=StandardScaler()
+       y_trainScaler=StandardScaler()
+       y_testScaler=StandardScaler()
+
+
+
+       X_trainScalerFit=X_trainScaler.fit(X_train)
+       X_testScalerFit=X_testScaler.fit(X_test)
+       y_trainScalerFit=y_trainScaler.fit(y_train)
+       y_testScalerFit=y_testScaler.fit(y_test)
+
+
+       X_train=X_trainScalerFit.transform(X_train)
+       X_test=X_testScalerFit.transform(X_test)
+       y_train=y_trainScalerFit.transform(y_train)
+       y_test=y_testScalerFit.transform(y_test)
+       
+       
+       #################
+       
+else:
+       cibles=st.radio(
+              "quelle est la variable cible que vous voulez!, ",
+              ('première','deuxième'),key=100000000001)
+       
+       
+       if cibles=='première':
+              col='ref1'
+       else:
+              col='ref2'
+       
+       X=df[columns].values
+       Y=df[col].values.reshape(-1, 1)
+   
+
+
+       PredictorScaler=StandardScaler()
+       TargetVarScaler=StandardScaler()
+
+
+       PredictorScalerFit=PredictorScaler.fit(X)
+       TargetVarScalerFit=TargetVarScaler.fit(Y)
+
+
+       X=PredictorScalerFit.transform(X)
+       Y=TargetVarScalerFit.transform(Y)
+
+
+
+       X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+       
+       
+st.markdown("On commence par standariser les données. ")   
+st.latex(r'''
+     X_{sta}=\frac{X-\mathbb{E}[X]}{sd[X]}
+     ''')
+st.markdown("On passe à la construction du modèle. ")
 st.markdown(" Veuillez choisir le nombre de couches internes de votre modèle: ")
 length=st.slider("Votre choix", step= 2,min_value=0, max_value=50,value= 50) 
 functions=['tanh','softmax','relu','softplus','softplus','hard_sigmoid','linear']
@@ -43,46 +128,7 @@ for i in range(length):
 st.write(units,activation)      
 #df=df=pd.read_csv('DB aout.csv')
 
-columns=[ 'S0_F0', 'S1_F0', 'S2_F0', 'S3_F0',
-       'S4_F0', 'S5_F0', 'S6_F0', 'S7_F0', 'S8_F0', 'S9_F0', 'S10_F0',
-       'S11_F0', 'S12_F0', 'S13_F0', 'S14_F0', 'S0_F1', 'S0_F2', 'S0_F3',
-       'S0_F4', 'S1_F1', 'S1_F2', 'S1_F3', 'S1_F4', 'S2_F1', 'S2_F2', 'S2_F3',
-       'S2_F4', 'S3_F1', 'S3_F2', 'S3_F3', 'S3_F4', 'S4_F1', 'S4_F2', 'S4_F3',
-       'S4_F4', 'S5_F1', 'S5_F2', 'S5_F3', 'S5_F4', 'S6_F1', 'S6_F2', 'S6_F3',
-       'S6_F4', 'S7_F1', 'S7_F2', 'S7_F3', 'S7_F4', 'S8_F1', 'S8_F2', 'S8_F3',
-       'S8_F4', 'S9_F1', 'S9_F2', 'S9_F3', 'S9_F4', 'S10_F1', 'S10_F2',
-       'S10_F3', 'S10_F4', 'S11_F1', 'S11_F2', 'S11_F3', 'S11_F4', 'S12_F1',
-       'S12_F2', 'S12_F3', 'S12_F4', 'S13_F1', 'S13_F2', 'S13_F3', 'S13_F4',
-       'S14_F1', 'S14_F2', 'S14_F3', 'S14_F4', 'S0_F5', 'S1_F5', 'S2_F5',
-       'S3_F5', 'S4_F5', 'S5_F5', 'S6_F5', 'S7_F5', 'S8_F5', 'S9_F5', 'S10_F5',
-       'S11_F5', 'S12_F5', 'S13_F5', 'S14_F5']
 
-
-X=df[columns].values
-Y1=df['ref1'].values.reshape(-1, 1)
-Y2=df['ref2'].values.reshape(-1, 1)
-
-
-
-
-PredictorScaler=StandardScaler()
-TargetVarScaler1=StandardScaler()
-TargetVarScaler2=StandardScaler()
- 
-
-PredictorScalerFit=PredictorScaler.fit(X)
-TargetVarScalerFit1=TargetVarScaler1.fit(Y1)
-TargetVarScalerFit2=TargetVarScaler2.fit(Y2)
- 
-
-X=PredictorScalerFit.transform(X)
-Y1=TargetVarScalerFit1.transform(Y1)
-Y2=TargetVarScalerFit2.transform(Y2)
- 
-
-
-X_train1, X_test1, y1_train, y1_test = train_test_split(X, Y1, test_size=0.3, random_state=42)
-X_train2, X_test2, y2_train, y2_test = train_test_split(X, Y2, test_size=0.3, random_state=42)
 
 class reg_model():
        
